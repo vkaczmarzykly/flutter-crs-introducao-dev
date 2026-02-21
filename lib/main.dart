add09bcb83a8f30a6aa8bc69b4af151eb6f11275
vkaczmarzykly <141168375+vkaczmarzykly@users.noreply.github.com>
2026-02-21 08:06:52 -0300

    aula02

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:front_end_flutter/widgets/widget_conteudo.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,8 +15,8 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
       ),
+      home: const WidgetConteudo(),
     );
   }
 }
